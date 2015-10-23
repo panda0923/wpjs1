e3ec8b7fcf09827dd35b04c89a54588e40e9bdee
bit-user <bit-user@bit>
2015-10-23 19:12:43 +0900

    1

---
+++ WebContent/hello.js
@@ -0,0 +1,8 @@
+// alert("hell world");// 화면에 hello world 띄우기(팝업창)
+ document.write("<h1>Hello World</h1>") //돔?(body,h1,p,em,등 트리구조를 돔이라고 한다.)
+// alert("!!!");
+//window.location.href="http://www.naver.com";
+  // alert("hell world");// 화면에 hello world 띄우기(팝업창)
+ // document.write("<h1>Hello World</h1>") //돔?(body,h1,p,em,등 트리구조를 돔이라고 한다.)
+ // alert("!!!");
+ // window.location.href="http://www.naver.com";
